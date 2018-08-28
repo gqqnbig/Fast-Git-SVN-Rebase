1e5e02250fc25331d497da06f70bb0899d2a57d4
Qiqi Gu <qiqig@meridianlink.com>
2018-08-28 16:17:36 -0700

    Able to recover from certain error states
    Output colorful messages. Red (\e[31m) indicates errors, yellow (\e[93m) indicates warnings.

--- sync.sh
+++ sync.sh
@@ -5,13 +5,23 @@ git pull origin trunk
 git checkout master
 git pull --ff-only origin master
 if [ $? -ne 0 ]; then
-	>&2 echo "Branch master diverges from origin/master. Please fix manully."
+	>&2 echo -e "\e[31mBranch master diverges from origin/master. Please fix manully.\e[0m"
+	>&2 echo -e "\e[31mYou may want to reset master to orgin/master.\e[0m"
 	exit 4
 fi
 
 if [ "$(git rev-list -1 trunk)" != "$(git rev-list -1 git-svn)" ]; then
-	>&2 echo "trunk doesn't align with git-svn!"
-	exit 1
+	>&2 echo -e "\e[93mtrunk doesn't align with git-svn!\e[0m"
+
+	if git merge-base --is-ancestor git-svn trunk; then
+		echo "git-svn is ancestor of trunk. Reset to git-svn."
+		# current branch is master, so I can directly move the pointer of trunk.
+		git branch -f trunk git-svn || exit 1
+		git push --force-with-lease origin trunk || exit 1
+	else
+		>&2 echo "git-svn is not ancestor of trunk."
+		exit 1
+	fi
 fi
 
 git svn rebase || exit 1

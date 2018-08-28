6ace89a60b30ada6cdf1c9d4ee221966f8e17a7b
Qiqi Gu <qiqig@meridianlink.com>
2018-08-28 15:43:30 -0700

    Improve robustness, only fast-forward master

--- sync.sh
+++ sync.sh
@@ -3,7 +3,11 @@ git config --local gc.auto 0
 git checkout trunk
 git pull origin trunk
 git checkout master
-git pull origin master
+git pull --ff-only origin master
+if [ $? -ne 0 ]; then
+	>&2 echo "Branch master diverges from origin/master. Please fix manully."
+	exit 4
+fi
 
 if [ "$(git rev-list -1 trunk)" != "$(git rev-list -1 git-svn)" ]; then
 	>&2 echo "trunk doesn't align with git-svn!"

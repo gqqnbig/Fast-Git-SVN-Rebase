ad25c65aa85355726b76de23f7b02f7e64d59f01
Qiqi Gu <qiqig@meridianlink.com>
2018-09-10 14:58:47 -0700

    WaitForExit and retrieve exit code BugzID: 283773

--- run-as.ps1
+++ run-as.ps1
@@ -1,3 +1,21 @@
 $password = $args[0] | ConvertTo-SecureString -asPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("QIQIGWRK2\git2svn",$password)
-Start-Process -Credential $credential -FilePath "C:\Program Files\Git\git-bash.exe" -ArgumentList '--cd=C:\LoansPQ2-Git-SVN C:\Temp\GitToSvn\sync.sh'
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = "C:\Program Files\Git\git-bash.exe"
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.Arguments =  'C:\Temp\GitToSvn\sync.sh'
+$pinfo.Domain = "."
+$pinfo.UserName="git2svn"
+$pinfo.Password = $password
+# The WorkingDirectory property must be set if UserName and Password are provided. https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.processstartinfo.username
+# If the property is not set, Start method will throw error "The directory name is invalid".
+$pinfo.WorkingDirectory = "C:\LoansPQ2-Git-SVN"
+
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+#Do Other Stuff Here....
+$p.WaitForExit()
+exit $p.ExitCode

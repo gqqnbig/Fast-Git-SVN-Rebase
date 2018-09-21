dcbec84b7c221620055953467bab4d023ef9f638
Qiqi Gu <qiqig@meridianlink.com>
2018-09-21 16:33:48 -0700

    Don't swallow output because I didn't write output stream to anywhere

--- sync.ps1
+++ sync.ps1
@@ -177,7 +177,7 @@ $password = $args[0] | ConvertTo-SecureString -asPlainText -Force
 $pinfo = New-Object System.Diagnostics.ProcessStartInfo
 $pinfo.FileName = "git"
 $pinfo.Arguments =  'svn dcommit'
-$pinfo.RedirectStandardError = $true; $pinfo.RedirectStandardOutput = $true; $pinfo.UseShellExecute = $false
+$pinfo.UseShellExecute = $false
 $pinfo.Domain = "."; $pinfo.UserName="git2svn"; $pinfo.Password = $password
 # The WorkingDirectory property must be set if UserName and Password are provided. https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.processstartinfo.username
 # If the property is not set, Start method will throw error "The directory name is invalid".
